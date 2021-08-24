fc975b112a8ba1fcdb2ef96696fe3fcaeff30463
Noor Raihan <blazerred71@gmail.com>
2021-08-25 00:10:34 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react'
+import { View, Text, SafeAreaView, Platform, StatusBar, ImageBackground } from 'react-native'
+import * as Font from 'expo-font'
+import {AppLoading} from 'expo'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import HomeScreen from './screens/HomeScreen'
+import StatisticScreen from './screens/StatisticScreen'
+import Symptoms from './screens/Symptoms'
+import About from './screens/About'
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <NavigationContainer> 
+        <Tab.Navigator barStyle={{ backgroundColor: '#EA0449' }} screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Statistic') {
+              iconName = 'stats-chart';
+            } else if(route.name === 'Symptoms') {
+              iconName = 'newspaper';
+            } else if(route.name === 'About') {
+              iconName = 'information-circle';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'white',
+          tabBarStyle: {
+            backgroundColor: '#EA0449'
+          },
+          headerShown: false
+        })}
+      >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Statistic" component={StatisticScreen} />
+          <Tab.Screen name="Symptoms" component={Symptoms} />
+          <Tab.Screen name="About" component={About} />
+        </Tab.Navigator>
+      </NavigationContainer>
+       );
+   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+   navContainer: {
+      backgroundColor: 'red',
+   },
+};
+
+export default App;
+
---
+++ screens/About.js
@@ -0,0 +1,189 @@
+import React, { Component, useState } from 'react'
+import { View, Text, SafeAreaView, Platform, StatusBar, ImageBackground } from 'react-native'
+import * as Font from 'expo-font'
+import {AboutLoading} from 'expo'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+
+
+const fetchFont = () => {
+  return Font.loadAsync({
+    'Heavitas': require('../assets/fonts/Heavitas.ttf'),
+  });
+};
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+class About extends Component {
+
+  //importing custom downloadable font
+  getCurrentDate(){
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    switch(month) {
+      case 1:
+        month = 'JANUARY';
+        break;
+
+      case 2:
+        month = 'FEBRUARY';
+        break;
+      
+      case 3:
+        month = 'MARCH';
+        break;
+      
+      case 4:
+        month = 'APRIL';
+        break;
+
+      case 5:
+        month = 'MAY';
+        break;
+      
+      case 6:
+        month = 'JUNE';
+        break;
+
+      case 7:
+        month = 'JULY';
+        break;
+      
+      case 8:
+        month = 'AUGUST';
+        break;
+      
+      case 9:
+        month = 'SEPTEMBER';
+        break;
+      
+      case 10:
+        month = 'OCTOBER';
+        break;
+      
+      case 11:
+        month = 'NOVEMBER';
+        break;
+      
+      case 12:
+        month = 'DECEMBER';
+        break;
+    }
+
+
+    //Alert.alert(date + '-' + month + '-' + year);
+    // You can turn it in to your desired format
+    return date + ' ' + month + ' ' + year;//format: dd-mm-yyyy;
+  }
+
+  render() {
+
+
+    return (
+           <View style={styles.container}>
+            <ImageBackground style={styles.topImg} source={require('../assets/virus.jpg')} resizeMode="cover">
+              <View style={styles.topBox}>
+                <Text style={styles.title}>ABOUT</Text>
+              </View>
+            </ImageBackground>
+            <View style={styles.statusBox}>
+              <Text style={styles.statusTitle2}>DEVELOPED BY NOOR RAIHAN</Text>
+            </View>
+           </View>
+       );
+   }
+}
+
+const styles = {
+   container: {
+      flex: 1,
+      alignItems: 'center',
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+   },
+
+   topBox: {
+    backgroundColor:'rgba(196,4,57,0.9)',
+    height: 350,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+   },
+
+   topImg: {
+     position:'absolute',
+     width: '100%',
+     zIndex: -1
+   },
+
+   title: {
+    color: '#FFFFFF',
+    // fontFamily: 'Heavitas',
+    paddingTop: '5%',
+    paddingLeft: '5%',
+    fontSize: 25,
+    fontWeight: 'bold'
+   },
+
+   statusBox: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    height: '100%',
+    width: '85%',
+    marginTop: '17%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    shadowRadius: 5,
+    elevation: 5,
+    borderRadius: 30,
+   },
+
+   detailBox: {
+    backgroundColor: 'white',
+    height: '40%',
+    width: '85%',
+    marginTop: '5%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    shadowRadius: 5,
+    elevation: 5,
+    borderRadius: 30,
+   },
+
+   statusTitle: {
+     textAlign: 'center',
+     fontSize: 20,
+     paddingTop: '5%',
+     fontWeight: 'bold',
+     color: '#5A5A5A'
+   },
+
+   statusTitle2: {
+    textAlign: 'left',
+    fontSize: 20,
+    paddingTop: '5%',
+    paddingLeft: '10%',
+    fontWeight: 'bold',
+    color: '#000'
+  },
+
+   subHead: {
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000'
+   }
+};
+
+export default About;
+
